99c4f1b788492e823c6f882bc6af8ccfcc39fbeb
funkycadet <ericsmithsonian2@gmail.com>
2023-01-02 18:39:03 +0100

    task: implement object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+export default function getBudgetObject(income, gdp, capita) {
+//   const budget = {
+//     income: income,
+//     gdp: gdp,
+//     capita: capita,
+//   };
+  let budget = { income, gdp, capita};
+
+  return budget;
+}

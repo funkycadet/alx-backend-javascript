c2f295ab6ffeb5242997bb6b2994acc044232d8e
funkycadet <ericsmithsonian2@gmail.com>
2023-01-04 06:06:16 +0100

    fix: resolve empty array

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,7 @@
 export default function getListStudentsIds(studentArray) {
+  if (!Array.isArray(studentArray)) {
+    return [];
+  }
   const studentId = studentArray.map(({ id }) => id);
   return studentId;
 }

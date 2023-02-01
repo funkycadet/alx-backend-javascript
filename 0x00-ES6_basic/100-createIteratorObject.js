5ca243848ebdf39b0d4ad802d332e8739ab57c6d
funkycadet <ericsmithsonian2@gmail.com>
2023-02-01 04:13:24 +0100

    task: iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  const result = [];
+  for (const i of Object.values(report.allEmployees)) {
+    result.push(...i)
+  }
+    return result;
+}

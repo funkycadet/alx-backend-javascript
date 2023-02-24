7124a18dd651abdd7fbec60314a242fc68d4ceba
funkycadet <ericsmithsonian2@gmail.com>
2023-02-24 16:23:40 +0100

    task: create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});

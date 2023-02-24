956e07a55733228d9ec455d2aa25b0a734ca41f5
funkycadet <ericsmithsonian2@gmail.com>
2023-02-24 03:17:30 +0100

    fix: resolve process.exit error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,14 +4,14 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+    const name = data.toString();
+    process.stdout.write(`Your name is: ${name}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+    const name = data.toString();
+    process.stdout.write(`Your name is: ${name}`);
     process.exit();
   });
   process.on('exit', () => {

f1df116372433d3eab9c0c6431d6bb4c15bf477f
funkycadet <ericsmithsonian2@gmail.com>
2023-02-27 05:59:31 +0100

    task: use stubs to process test cases

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,13 +1,14 @@
 const { describe, it } = require("mocha");
 const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./3-payment");
+const sendPaymentRequestToApi = require("./4-payment");
 const Utils = require("./utils");
 const assert = require("assert");
 
 describe("sendPaymentRequestToApi", function () {
   it("check that Utils.calculateNumber was called once", function () {
     const spy = sinon.spy(Utils, "calculateNumber");
-    sendPaymentRequestToApi(2350, 2452);
+    const stub = sinon.stub(Utils, "calculateNumber").returns(10);
+    sendPaymentRequestToApi(1000, 20);
 
     assert(spy.withArgs("The total is: 10").calledOnce);
     assert(stub.withArgs("SUM", 100, 20).calledOnce);

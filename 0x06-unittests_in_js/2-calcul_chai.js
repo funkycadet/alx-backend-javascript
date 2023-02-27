b13a4beb900978e2771a9fb09e58093c24536e7f
funkycadet <ericsmithsonian2@gmail.com>
2023-02-27 05:12:34 +0100

    task: basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,22 @@
+function calculateNumber(type, a, b) {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+    let c = 0;
+    switch (type) {
+    case 'SUM':
+        return roundedA + roundedB;
+        break;
+    case 'SUBTRACT':
+        return roundedA - roundedB;
+        break;
+    case 'DIVIDE':
+      if (roundedB === 0) {
+            return "Error";
+      } else {
+            return roundedA / roundedB;
+      }
+      break;
+    }
+}
+
+module.exports = calculateNumber;
